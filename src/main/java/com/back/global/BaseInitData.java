ab84e2efc81a74994fb93d63fe470fdd1d3c0f17
5s-mini <dnclsehd123@naver.com>
2026-02-23 10:43:18 +0900

    feat: SQL 로그 출력 정보 상세화

--- src/main/java/com/back/global/BaseInitData.java
+++ src/main/java/com/back/global/BaseInitData.java
@@ -16,10 +16,17 @@ public class BaseInitData {
     @Bean
     ApplicationRunner initDataRunner() {
         return args -> {
+
+            if(postRepository.count() > 0) {
+                return;
+            }
+
             System.out.println("초기 데이터를 로딩합니다.");
 
             Post post1 = new Post("제목1", "내용1");
             postRepository.save(post1);
+
         };
     }
+
 }
