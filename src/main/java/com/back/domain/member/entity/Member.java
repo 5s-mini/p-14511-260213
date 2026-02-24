5cefe891f5fbe203a396340076158903f8218e2e
5s-mini <dnclsehd123@naver.com>
2026-02-24 12:25:28 +0900

    feat: 회원 도메인 추가, 테스트 유저 데이터 5개 등록

--- src/main/java/com/back/domain/member/entity/Member.java
+++ src/main/java/com/back/domain/member/entity/Member.java
@@ -3,8 +3,12 @@ package com.back.domain.member.entity;
 import com.back.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
 public class Member extends BaseEntity {
 
     @Column(unique = true)
@@ -13,4 +17,5 @@ public class Member extends BaseEntity {
     private String password;
     @Column(length = 30)
     private String nickname;
+
 }
